37e951efba9a60bdb1bb8b2ca8e98d89a7938c2d
Dan <uprise419@gmail.com>
2026-02-20 13:31:33 +0700

    memperbaiki auth

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,10 +1,22 @@
-import { Link } from 'react-router-dom';
-import { FiMessageSquare, FiBarChart2, FiLogIn } from 'react-icons/fi';
+import { Link, useNavigate } from "react-router-dom";
+import { FiMessageSquare, FiBarChart2, FiLogIn, FiLogOut } from "react-icons/fi";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../features/auth/authSlice";
 
 function BottomNav() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { token } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/"); // kembali ke home
+  };
+
   return (
     <footer className="fixed bottom-0 left-0 w-full border-t bg-white py-4 
-  flex justify-center gap-x-6 text-sm z-50">
+      flex justify-center gap-x-6 text-sm z-50">
 
       <Link to="/" className="flex flex-col items-center text-gray-700 hover:text-purple-600">
         <FiMessageSquare size={22} />
@@ -16,10 +28,23 @@ function BottomNav() {
         <span className="text-xs mt-1">Leaderboards</span>
       </Link>
 
-      <Link to="/login" className="flex flex-col items-center text-gray-700 hover:text-purple-600">
-        <FiLogIn size={22} />
-        <span className="text-xs mt-1">Login</span>
-      </Link>
+      {!token ? (
+        <Link
+          to="/login"
+          className="flex flex-col items-center text-gray-700 hover:text-purple-600"
+        >
+          <FiLogIn size={22} />
+          <span className="text-xs mt-1">Login</span>
+        </Link>
+      ) : (
+        <button
+          onClick={handleLogout}
+          className="flex flex-col items-center text-gray-700 hover:text-red-600"
+        >
+          <FiLogOut size={22} />
+          <span className="text-xs mt-1">Logout</span>
+        </button>
+      )}
 
     </footer>
   );
