8eadd359833d5f0569f7f76500e4c5222e7e28cd
Dan <uprise419@gmail.com>
2026-03-12 22:34:48 +0700

    menyelesaikan ci/cd

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: 'http://localhost:5173',
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config;
+    },
+
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
